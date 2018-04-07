8c931cda6919a02acf77ed06e7600da92e306628
himanshu ranjan <hranjan552@gmail.com>
2018-04-07 09:56:25 +0530

    socket.io added

--- server/app.js
+++ server/app.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 // Set static folder
 app.use(express.static(path.join(__dirname, '../public')));
 
+io.on('connection', (socket) => {
+    console.log('New user connected');
+  
+    socket.on('disconnect', () => {
+      console.log('User was disconnected');
+    });
+  });
+  
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server started on port ${port}`)
 });
