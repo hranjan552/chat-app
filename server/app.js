662e45df36cdb3daee0cd230f31305da9dd40dd6
himanshu ranjan <hranjan552@gmail.com>
2018-04-07 11:35:07 +0530

    greet new user and alert other

--- server/app.js
+++ server/app.js
@@ -14,41 +14,44 @@ io.on('connection', (socket) => {
 
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
-        
+
         console.log('createMessage', message);
 
         io.emit('newMessage', {
-          from: message.from,
-          text: message.text,
-          createdAt: new Date().getTime()
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
         });
         // socket.broadcast.emit('newMessage', {
         //   from: message.from,
         //   text: message.text,
         //   createdAt: new Date().getTime()
         // });
-      });
+    });
 
-    // socket.emit('newMessage', {
-    //     from: 'himanshu',
-    //     text: 'hello from himanshu',
-    //     createdAt: 6543276
-    //   });
-
-    //   socket.on('createMessage', (message) => {
-    //     console.log('createMessage', message);
-    //   });
 
     socket.on('disconnect', () => {
-      console.log('User was disconnected');
+        console.log('User was disconnected');
     });
-  });
+});
 
 
 
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-  console.log(`Server started on port ${port}`)
+    console.log(`Server started on port ${port}`)
 });
